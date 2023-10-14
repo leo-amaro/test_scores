5e7382872401f8b6ad23689afb79ae847ea27f8d
leo-amaro-conestoga <lamaro3722@conestogac.on.ca>
2023-10-14 15:22:27 -0400

    final

--- test_scores.js
+++ test_scores.js
@@ -1,52 +1,106 @@
-"use strict";
+$(document).ready(function () {
+  // Initialize arrays for names and scores
+  const names = ["Ben", "Joel", "Judy", "Anne"];
+  const scores = [88, 98, 77, 88];
 
-const names = ["Ben", "Joel", "Judy", "Anne"];
-const scores = [88, 98, 77, 88];
+  // Function to add a score
+  const addScore = () => {
+    const name = $("#name").val();
+    const score = parseInt($("#score").val());
+    let isValid = true;
 
-const $ = (selector) => document.querySelector(selector);
+    // Check if the name field is empty
+    if (name === "") {
+      $("#name").next().text("This field is required.");
+      isValid = false;
+    } else {
+      $("#name").next().text("");
+    }
 
-const addScore = () => {
-  // get user entries
-  const name = $("#name").value;
-  const score = parseInt($("#score").value);
-  let isValid = true;
+    // Check if the score is valid
+    if (isNaN(score) || score < 0 || score > 100) {
+      $("#score").next().text("You must enter a score between 0 and 100.");
+      isValid = false;
+    } else {
+      $("#score").next().text("");
+    }
 
-  // check entries for validity
-  if (name == "") {
-    $("#name").nextElementSibling.textContent = "This field is required.";
-    isValid = false;
-  } else {
-    $("#name").nextElementSibling.textContent = "";
-  }
+    if (isValid) {
+      // Add the name and score to the arrays
+      names.push(name);
+      scores.push(score);
 
-  if (isNaN(score) || score < 0 || score > 100) {
-    $("#score").nextElementSibling.textContent =
-      "You must enter a valid score.";
-    isValid = false;
-  } else {
-    $("#score").nextElementSibling.textContent = "";
-  }
+      // Clear the input fields
+      $("#name").val("");
+      $("#score").val("");
+      $("#scores_display").val("");
 
-  if (isValid) {
-    names[names.length] = name;
-    scores[scores.length] = score;
-    $("#name").value = "";
-    $("#score").value = "";
-  }
+      // Display an alert when successfully added new name and score
+      $("#success-alert").show();
+      $("#alert-msg").html(
+        "You added: Name: <b>" +
+          name +
+          "</b> and " +
+          "Score: <b>" +
+          score +
+          "</b>"
+      );
+      setTimeout(function () {
+        $("#success-alert").fadeOut(1000);
+      }, 2500);
+    }
+    $("#name").focus();
+  };
+
+  //function to combine the arrays
+  const combineArr = (names, scores) => {
+    const combinedData = names.map((name, index) => ({
+      name,
+      score: scores[index],
+    }));
+
+    return combinedData;
+  };
+
+  //format the combined arrays
+  const formatSortedData = (sortedData) => {
+    const output = sortedData
+      .map((item) => `${item.name}: ${item.score}`)
+      .join("\n");
+    return output;
+  };
+
+  //sort the combined arrays by descending
+  const sortArrByDesc = (combinedData) => {
+    const sortedData = combinedData.slice().sort((a, b) => b.score - a.score);
+    return sortedData;
+  };
+
+  //sort the combined arrays by descending
+  const sortArrByAsc = (combinedData) => {
+    const sortedData = combinedData.slice().sort((a, b) => a.score - b.score);
+    return sortedData;
+  };
+
+  // Function to display name and score in textarea
+  const displayScores = () => {
+    const sortOption = document.getElementById("sort_scores").value;
+    const combinedData = combineArr(names, scores);
+
+    let sortedData;
+    if (sortOption === "highest_to_lowest") {
+      sortedData = sortArrByDesc(combinedData);
+    } else if (sortOption === "lowest_to_highest") {
+      sortedData = sortArrByAsc(combinedData);
+    }
+
+    $("#scores_display").val(formatSortedData(sortedData));
+  };
+
+  // Attach event handlers to elements
+  $("#add").on("click", addScore);
   $("#name").focus();
-
-  //clear the textarea after adding item in array
-  $("#scores_display").value = "";
-};
-
-// function to display the names and scores
-const displayScores = () => {
-  const output = names.map((n, i) => `${n}: ${scores[i]}`).join("\n");
-  $("#scores_display").value = output;
-};
-
-document.addEventListener("DOMContentLoaded", () => {
-  $("#add").addEventListener("click", addScore);
-  $("#name").focus();
-  $("#display_scores").addEventListener("click", displayScores);
+  $("#display_scores").on("change", displayScores);
+  $("#display_scores").on("click", displayScores);
+  $("#sort_scores").on("change", displayScores);
 });
